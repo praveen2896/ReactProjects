68f3853a43f3b42ada30fde8bec3997003ce35c8
praveen2896 <mpraveenkumarown@gmail.com>
2018-09-09 11:00:08 +0530

    changes

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import Button from'./Buttons/clickbuttons'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-     
+        <h1>hi i am class component</h1>
+        <div >
+          <table>
+            <tr>
+              <td>
+                <Button value="123"></Button>
+               </td>
+               <td> 
+                <Button value="123"></Button>
+              </td>
+            </tr>
+          </table>
+        </div>
       </div>
     );
   }
